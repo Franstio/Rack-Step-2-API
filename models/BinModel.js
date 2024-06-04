fbdb637090869533811d2fdb32be24d678e6a405
FahriEfendi <fahriefendi0795@gmail.com>
2024-06-04 13:43:31 +0700

    .

--- models/BinModel.js
+++ models/BinModel.js
@@ -38,7 +38,7 @@ const Bin = db.define('bin', {
 });
 
 
-Waste.hasMany(Bin, { foreignKey: 'IdWaste', as: 'dataWaste' });
-Bin.belongsTo(Waste, { foreignKey: 'IdWaste', as: 'dataWaste' });
+Waste.hasMany(Bin, { foreignKey: 'IdWaste', as: 'bin' });
+Bin.belongsTo(Waste, { foreignKey: 'IdWaste', as: 'waste' });
 
 export default Bin;
