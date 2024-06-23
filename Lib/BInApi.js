e1038711189b5fdf9956ad4acb78cca31bf003e6
Franstio <franstiodiego01@gmail.com>
2024-06-23 17:55:00 +0700

    Update

--- Lib/BInApi.js
+++ Lib/BInApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const apiClient = axios.create({
     withCredentials: false
 });
+const rackTarget = "PCS-02.local";
 export const setRackDoor = async (id,address,open)=>{
     return await apiClient.post(`http://${rackTarget}/rackOpenManual`,{
         idRack: binData.dataValues.id,
