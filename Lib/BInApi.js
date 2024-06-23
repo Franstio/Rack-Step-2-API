8b766fffb084ad343f63f56f56fd3b42f138c243
Franstio <franstiodiego01@gmail.com>
2024-06-23 17:58:20 +0700

    Update

--- Lib/BInApi.js
+++ Lib/BInApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const apiClient = axios.create({
     withCredentials: false
 });
-const rackTarget = "PCS-02.local:5001";
+const rackTarget = "PCS-02.local:5000";
 export const setRackDoor = async (id,address,open)=>{
     return await apiClient.post(`http://${rackTarget}/rackOpenManual`,{
         idRack: id,
