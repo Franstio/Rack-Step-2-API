7186a47918c28b1f7800f053790f71c5a1af2bd8
Franstio <franstiodiego01@gmail.com>
2025-01-02 16:36:48 +0700

    ok

--- controllers/Scales.js
+++ controllers/Scales.js
@@ -1,51 +1,51 @@
-import { SerialPort } from 'serialport';
-const Timbangan = new SerialPort({
-    path: '/dev/ttyUSB0',
-    baudRate: 9600,
-    dataBits: 8,
-    stopBits: 1,
-    parity: 'none',
-}); 
+// import { SerialPort } from 'serialport';
+// const Timbangan = new SerialPort({
+//     path: '/dev/ttyUSB0',
+//     baudRate: 9600,
+//     dataBits: 8,
+//     stopBits: 1,
+//     parity: 'none',
+// }); 
 
-Timbangan.on('error', (error) => {
-});
+// Timbangan.on('error', (error) => {
+// });
 
-let _50kgOutput = '';
-export const getScales50Kg = (io) => {
-    try {
-        let response;
-         setInterval(function(){
-             response = { weight50Kg: 20 };
-             io.emit('data', response);
-         },5000); 
-        io.on('connectScale', () => {
-            Timbangan.open(() => {
-            });
-        });
-        Timbangan.on('data', (data) => {
-            /*if (data.toString()!='\n')
-            {
-                _50kgOutput = _50kgOutput+ data.toString();
-                return;
-            }*/
-            _50kgOutput = data.toString().replace("\r","").replace("\n","");
-            const match = _50kgOutput.toString().match(/[\d]+\.\d{2}(?=Kg)/);
+// let _50kgOutput = '';
+// export const getScales50Kg = (io) => {
+//     try {
+//         let response;
+//          setInterval(function(){
+//              response = { weight50Kg: 20 };
+//              io.emit('data', response);
+//          },5000); 
+//         io.on('connectScale', () => {
+//             Timbangan.open(() => {
+//             });
+//         });
+//         Timbangan.on('data', (data) => {
+//             /*if (data.toString()!='\n')
+//             {
+//                 _50kgOutput = _50kgOutput+ data.toString();
+//                 return;
+//             }*/
+//             _50kgOutput = data.toString().replace("\r","").replace("\n","");
+//             const match = _50kgOutput.toString().match(/[\d]+\.\d{2}(?=Kg)/);
 
-            if (match) {
-                const weight = match[0];
-                response = { weight: parseFloat(weight) };
-                response = { weight50Kg: weight };
-                io.emit('data1', response);
-            }
-            _50kgOutput = '';
-        });
+//             if (match) {
+//                 const weight = match[0];
+//                 response = { weight: parseFloat(weight) };
+//                 response = { weight50Kg: weight };
+//                 io.emit('data1', response);
+//             }
+//             _50kgOutput = '';
+//         });
 
-       Timbangan.on('error', (error) => {
-        }); 
-        if (response != undefined && response != null) {
-            res.status(200).json(response);
-        }
-    } catch (error) {
-        //    res.status(500).json({ msg: error.message });
-    }
-};
+//        Timbangan.on('error', (error) => {
+//         }); 
+//         if (response != undefined && response != null) {
+//             res.status(200).json(response);
+//         }
+//     } catch (error) {
+//         //    res.status(500).json({ msg: error.message });
+//     }
+// };
